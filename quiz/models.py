7007a6ecd3cb3c3126b3ec18a40c6c5cc2d46ad3
khadija-556 <khatukhadija556@gmail.com>
2026-09-07 01:25:11 +0600

    Add Option model

--- quiz/models.py
+++ quiz/models.py
@@ -23,3 +23,16 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+
+class Option(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name="options"
+    )
+    option_text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.option_text
