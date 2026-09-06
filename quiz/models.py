1b45005899ac213d4a1b15acbc0ad4da4466beb3
khadija-556 <khatukhadija556@gmail.com>
2026-09-07 01:22:15 +0600

    Add Quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+
